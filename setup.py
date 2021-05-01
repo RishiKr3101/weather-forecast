91a4faea5bbf1b3c7209c26151a2e89b36015f2c
RishiKr3101 <rishikrofficial@gmail.com>
2021-05-01 21:53:29 +0530

    Add

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
-setup(name='weather_forecast',
-      version='0.45',
+setup(name='w-forecast',
+      version='0.0.1',
       description='Location based weather forecast package',
       long_description= 'The pip package provides weather forecasting information based on location or address. Using address, the weather_forecast provides location specific forecast. Currently only one function is included i.e forecast.',
       url='https://github.com/RishiKr3101/weather-forecast.git',
       author='Rishi',
       author_email='rishikrofficial@gmail.com',
-      license='MIT',
-      packages=['weather-forecast'],
+      packages=['w-forecast'],
       install_requires=[
           'geopy',
       ],
